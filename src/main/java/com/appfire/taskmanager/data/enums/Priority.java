7d05be45f796a6bc68ee0d3b2aa1288aa27fa16d
Peter Stoilkov <peter_sto@mail.bg>
2023-06-28 12:47:00 +0300

    Add JsonCreator for enums

--- src/main/java/com/appfire/taskmanager/data/enums/Priority.java
+++ src/main/java/com/appfire/taskmanager/data/enums/Priority.java
@@ -1,6 +1,9 @@
 package com.appfire.taskmanager.data.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Priority {
+
     HIGH("HIGH"),
     MEDIUM("MEDIUM"),
     LOW("LOW");
@@ -13,6 +16,18 @@ public enum Priority {
 
     public String getPriority() {
         return this.priority;
+    }
+
+    @JsonCreator
+    public static Priority getPriorityName(String value) {
+
+        for (Priority priority : Priority.values()) {
+            if (priority.getPriority().equals(value)) {
+                return priority;
+            }
+        }
+
+        return null;
     }
 
 }
--- src/main/java/com/appfire/taskmanager/data/enums/Status.java
+++ src/main/java/com/appfire/taskmanager/data/enums/Status.java
@@ -1,6 +1,9 @@
 package com.appfire.taskmanager.data.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Status {
+
     NEW("NEW"),
     IN_PROGRESS("IN PROGRESS"),
     DONE("DONE");
@@ -13,6 +16,18 @@ public enum Status {
 
     public String getStatus() {
         return this.status;
+    }
+
+    @JsonCreator
+    public static Status getStatusName(String value) {
+
+        for (Status status : Status.values()) {
+            if (status.getStatus().equals(value)) {
+                return status;
+            }
+        }
+
+        return null;
     }
 
 }
